688708a5d85bea3af0f722b5618e3a46398c5772
RiotIsAbadCompany <issos354@gmail.com>
2022-11-23 16:47:12 +0200

    100%

--- rep/Form5.Designer.cs
+++ rep/Form5.Designer.cs
@@ -66,6 +66,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -75,6 +76,7 @@
             this.button4.TabIndex = 3;
             this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -84,6 +86,7 @@
             this.button5.TabIndex = 4;
             this.button5.Text = "button5";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -93,6 +96,7 @@
             this.button6.TabIndex = 5;
             this.button6.Text = "button6";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // dataGridView1
             // 
--- rep/Form5.cs
+++ rep/Form5.cs
@@ -53,14 +53,12 @@ namespace rep
         void view2()
         {
 
-            string mySelect = "Select [Repair].Client_id,[Client].Client_name,[Repair].Repair_id,[Repair].Car_id,Car.Car_make,Car.Reg_number,[Repair].In_date FROM( [Repair] INNER JOIN" +
-                " Client " +
-                "ON [Client].Client_id = [Repair].Client_id" +
-                ") INNER JOIN" +
-                " Car" +
-                " ON Car.Car_id = [Repair].Car_id" +
-                " WHERE In_date >= (Now - 1)" +
-                " ORDER BY Car_make, Reg_number ASC";
+            string mySelect = "SELECT Car.Car_id, Car.Car_make, Car.Repair_price FROM Car" +
+                " WHERE Repair_price = ( SELECT MAX(Repair_price) FROM Car)" +
+                " union all" +
+                " SELECT Car.Car_id, Car.Car_make, Car.Repair_price FROM Car" +
+                " WHERE Repair_price = ( SELECT MIN(Repair_price) FROM Car)" +
+                " ORDER BY Car_make";
             dbConnect.ConnectionString = conStr;
             dbConnect.Open();
             OleDbDataAdapter adapt = new OleDbDataAdapter(mySelect, dbConnect);
@@ -70,6 +68,69 @@ namespace rep
             dbConnect.Close();
 
         }
+        void view3()
+        {
+
+            string mySelect = "SELECT Car.Car_id, [Repair].Repair_id, [Repair].Paid, Car.Reg_number, Car.Car_make FROM [Repair]" +
+                " INNER JOIN Car ON Car.Car_id = [Repair].Car_id" +
+                " WHERE Paid = FALSE";
+            dbConnect.ConnectionString = conStr;
+            dbConnect.Open();
+            OleDbDataAdapter adapt = new OleDbDataAdapter(mySelect, dbConnect);
+            DataTable dt = new DataTable();
+            adapt.Fill(dt);
+            dataGridView1.DataSource = dt;
+            dbConnect.Close();
+
+        }
+        void view4()
+        {
+
+            string mySelect = "SELECT Car.Car_id, [Repair].Repair_id, [Repair].Paid, Car.Reg_number, Car.Car_make FROM [Repair]" +
+                " INNER JOIN Car ON Car.Car_id = [Repair].Car_id" +
+                " WHERE Paid = TRUE";
+            dbConnect.ConnectionString = conStr;
+            dbConnect.Open();
+            OleDbDataAdapter adapt = new OleDbDataAdapter(mySelect, dbConnect);
+            DataTable dt = new DataTable();
+            adapt.Fill(dt);
+            dataGridView1.DataSource = dt;
+            dbConnect.Close();
+
+        }
+        void view5()
+        {
+
+            string mySelect = "SELECT TOP 3 [Repair].Car_id, COUNT([Repair].Car_id) AS MOST_FREQUENT FROM [Repair]" +
+                " GROUP BY [Repair].Car_id" +
+                " ORDER BY COUNT([Repair].Car_id) DESC";
+            dbConnect.ConnectionString = conStr;
+            dbConnect.Open();
+            OleDbDataAdapter adapt = new OleDbDataAdapter(mySelect, dbConnect);
+            DataTable dt = new DataTable();
+            adapt.Fill(dt);
+            dataGridView1.DataSource = dt;
+            dbConnect.Close();
+
+        }
+        void view6()
+        {
+
+            string mySelect = "SELECT TOP 1 Client.Client_id, SUM(Car.Repair_price) AS Repair_Sum" +
+                " FROM([Repair] INNER JOIN [Client] ON [Client].Client_id = [Repair].Client_id )" +
+                " INNER JOIN Car ON Car.Car_id = [Repair].Car_id" +
+                " GROUP BY [Client].Client_id" +
+                " ORDER BY SUM(Car.Repair_price) DESC";
+            dbConnect.ConnectionString = conStr;
+            dbConnect.Open();
+            OleDbDataAdapter adapt = new OleDbDataAdapter(mySelect, dbConnect);
+            DataTable dt = new DataTable();
+            adapt.Fill(dt);
+            dataGridView1.DataSource = dt;
+            dbConnect.Close();
+
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             view1();
@@ -78,6 +139,26 @@ namespace rep
         private void button2_Click(object sender, EventArgs e)
         {
             view2();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            view3();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            view4();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            view5();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            view6();
         }
     }
 }
